2d3f8e20461a045633871048fd39eac16cbb1590
smallint <tahoma@gmx.de>
2014-11-26 01:11:54 +0100

    [imx] @wolfgar return 3 in LinuxRendererGLES::GetOptimalBufferSize for IMX render path

--- xbmc/cores/VideoRenderers/LinuxRendererGLES.cpp
+++ xbmc/cores/VideoRenderers/LinuxRendererGLES.cpp
@@ -2977,7 +2977,7 @@ unsigned int CLinuxRendererGLES::GetOptimalBufferSize()
      m_format == RENDER_FMT_MEDIACODEC)
     return 2;
   else if(m_format == RENDER_FMT_IMXMAP)
-    return 1;
+    return 3;
   else
     return 3;
 }
