63f6e8b77736e9168c165c5cd798c7cb695dc8ef
smallint <tahoma@gmx.de>
2015-01-04 14:18:43 +0000

    Added CGUIWindowFullScreen::ClearBackground override for non gui video rendering

--- xbmc/video/windows/GUIWindowFullScreen.h
+++ xbmc/video/windows/GUIWindowFullScreen.h
@@ -30,6 +30,7 @@ public:
   virtual ~CGUIWindowFullScreen(void);
   virtual bool OnMessage(CGUIMessage& message);
   virtual bool OnAction(const CAction &action);
+  virtual void ClearBackground();
   virtual void FrameMove();
   virtual void Process(unsigned int currentTime, CDirtyRegionList &dirtyregion);
   virtual void Render();
